ae8d9c3f806f4e66d3ccc3fa15c38c614783cb73
Christopher Bandurski <cbandurski1@gmail.com>
2023-12-13 11:49:40 -0600

    test functionality

--- src/test/java/net/crusadergames/bugwars/BugWarsApplicationTests.java
+++ src/test/java/net/crusadergames/bugwars/BugWarsApplicationTests.java
@@ -3,11 +3,18 @@ package net.crusadergames.bugwars;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.springframework.test.util.AssertionErrors.fail;
+
 @SpringBootTest
 class BugWarsApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void alwaysFail() {
+		fail("always fail");
 	}
 
 }
