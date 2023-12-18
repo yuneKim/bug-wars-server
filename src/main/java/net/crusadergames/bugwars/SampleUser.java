98f32d54a71c9455bfb7b4e9a85b857374523d37
Christopher Bandurski <cbandurski1@gmail.com>
2023-12-17 18:18:06 -0600

    tests

--- src/main/java/net/crusadergames/bugwars/SampleUser.java
+++ src/main/java/net/crusadergames/bugwars/SampleUser.java
@@ -5,10 +5,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
---
+++ src/test/java/net/crusadergames/bugwars/SampleControllerTests.java
@@ -0,0 +1,64 @@
+package net.crusadergames.bugwars;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatcher;
+import org.mockito.ArgumentMatchers;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@WebMvcTest(controllers = SampleController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+public class SampleControllerTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SampleService sampleService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void getAll_returnsAllUsers() throws Exception {
+        SampleUser user = SampleUser.builder().username("Susan").build();
+        when(sampleService.getAll()).thenReturn(List.of(user));
+
+        ResultActions response = mockMvc.perform(get("/users"));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()", CoreMatchers.is(1)));
+    }
+
+    @Test
+    public void addUser_returnsCreated() throws Exception {
+        SampleUser user = SampleUser.builder().username("Charlie").build();
+        given(sampleService.addUser(ArgumentMatchers.any())).willAnswer(invocation -> invocation.getArgument(0));
+
+        ResultActions response = mockMvc.perform(post("/users/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(user)));
+
+        response.andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.username", CoreMatchers.is(user.getUsername())));
+    }
+}
