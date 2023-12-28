47431c325e4a21462b61c64b764fa13dd442a264
Christopher Bandurski <cbandurski1@gmail.com>
2023-12-28 16:21:04 -0600

    hopefully fixing auto table generation

--- src/main/java/net/crusadergames/bugwars/model/SampleString.java
+++ src/main/java/net/crusadergames/bugwars/model/SampleString.java
@@ -1,15 +1,13 @@
 package net.crusadergames.bugwars.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "sample_strings")
 @Builder
 @Data
 @NoArgsConstructor
