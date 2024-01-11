7867c2f1dc00e44cf94bc0e28f15f431f284c285
Christopher Bandurski <cbandurski1@gmail.com>
2024-01-11 13:55:04 -0600

    removed sample stuff

--- src/main/java/net/crusadergames/bugwars/repository/SampleStringRepository.java
+++
@@ -1,9 +0,0 @@
-package net.crusadergames.bugwars.repository;
-
-import net.crusadergames.bugwars.model.SampleString;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SampleStringRepository extends JpaRepository<SampleString, Long> {
-}
